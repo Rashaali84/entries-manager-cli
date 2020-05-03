8303b74e282af830c3b533f143f2496ce1404162
Rasha ali <rashaali84@gmail.com>
2020-05-03 22:33:37 +0200

    final touches to the solution

--- index.js
+++ index.js
@@ -1,140 +1,157 @@
 // require dependencies
-_;
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs'); //`npm i yargs` -- > update package.json + node_module
 
 // declare constants
-const ENTRIES_PATH = _;
-const DOC_STRING = `
-COMMANDS:
+const ENTRIES_PATH = 'entries.json';
 
-  all
-    print all entries to the console
+const argv = yargs
+  .command('all', 'print all entries to the console')
+  .command('read', ' print a single key/value pair to the console , use ex : read -k [key]', {
+    key: {
+      description: 'the key to read ',
+      alias: 'k',
+      type: 'string',
+    }
+  })
+  .command('write', ' write <key> <value>set the given key / value pair ex: write -k [key] -v [value]', {
+    key: {
+      description: 'the key to write ',
+      alias: 'k',
+      type: 'string',
+    },
+    value: {
+      description: 'the value to write ',
+      alias: 'v',
+      type: 'string',
+    }
+  }
+  )
+  .command('delete', 'delete <key>remove the entry with this key ex: delete -k [key]',
+    {
+      key: {
+        description: 'the key to delete ',
+        alias: 'k',
+        type: 'string',
+      }
+    })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-  read <key>
-    print a single key/value pair to the console
 
-  delete <key>
-    remove the entry with this key
-
-  write <key> <value>
-    set the given key/value pair
-
-FLAGS:
-
-  -h
-    print this helpful message
-`;
-
-
-// --- begin main script ---
-
-
-// step 0: log the docs for
-if (process.argv.includes('-h')) {
-  console.log(_);
-  // this line tells Node to stop right now, done, end, finished.
-  //  it's kind of like an early return, but for a node app
-  process.exit(0);
+if (argv._.includes('all')) {
+  console.log('reading all enteries ...');
+  fs.readFile(path.join(__dirname, ENTRIES_PATH), 'utf-8',
+    (err, contents) => {
+      if (err) throw err;
+      console.log(contents);
+      process.exit(0);
+    });
 }
 
-// step 1: declare main app function
-const entriesManager = (entries, command, key, value) => {
-
-  // step 5: make sure command is defined
-  //  alert the user and exit early if it is not
-  if (_) {
-    console.log(`a command is required  \nSee "node file.js -h"`);
-    _;
+// read -k [key]
+if (argv._.includes('read')) {
+  if (argv.key === undefined) {
+    console.log("A key is needed");
+    process.exit(0);
   }
+  console.log('reading enteries ...');
+  fs.readFile(path.join(__dirname, ENTRIES_PATH), 'utf-8',
+    (err, contents) => {
+      if (err) throw err;
+      console.log(contents);
+      const parsedContent = JSON.parse(contents);
+      if (parsedContent[argv.key]) {
+        console.log('value is = ' + parsedContent[argv.key]);
+        process.exit(0);
+      }
+      else {
+        console.log('No such a key found  ...');
+        process.exit(0);
 
-  // step 6: make sure the first argument is one of the 4 supported commands
-  //  alert the user and exit early if it is not
-  if (_) {
-    console.log(`${command} is not a valid command  \nSee "node file.js -h"`);
-    _;
+      }
+    });
+}
+//write -k [key] -v [value]
+if (argv._.includes('write')) {
+  if (argv.key === undefined) {
+    console.log("A key is needed");
+    process.exit(0);
   }
-
-  // step 7: log all entries if the user passed the 'all' command
-  //  this command does not require any other user arguments
-  //  exit early since there are no changes to save
-  if (_) {
-    _;
-    _;
+  if (argv.value === undefined) {
+    console.log("A value is needed");
+    process.exit(0);
   }
-
-  // step 8: all remaining commands require at least a key
-  //  alert the user and exit early if there is no key
-  if (key === undefined) {
-    console.log(`a key is required, cannot ${command}  \nSee "node file.js -h"`);
-    _;
-  }
-
-  // step 9: conditionally execute the logic for the remaining commands
-  if (_) {
-    // step 9 a.1: make sure the key exists before trying to read it
-    //  alert the user and exit early if it does not
-    if (_) {
-      console.log(`key "${key}" does not exist. cannot read`);
-      _;
+  fs.readFile(path.join(__dirname, ENTRIES_PATH), function readFileCallback(err, data) {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      obj = JSON.parse(data); //now it an object
+      console.log(obj);
+      obj[argv.key] = argv.value; //add some data
+      json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile(path.join(__dirname, ENTRIES_PATH), json, 'utf8', (err) => {
+        if (err)
+          throw err;
+        console.log(json);
+        console.log('insertion completed..');
+      }); // write it back 
     }
+  });
 
-    // step 9 a.2: print the requested entry
-    //  exit early, there are no changes to save
-    console.log(`${key}: ${entries[key]}`);
-    _;
+}
+// delete -k [key]
 
-  } else if (_) {
-    // step 9 b.1: make sure the key exists before trying to delete it
-    //  alert the user and exit early if it does not
-    if (_) {
-      console.log(`key "${key}" does not exist. cannot delete`);
-      _;
-    }
-
-    // step 9 b.2: delete the correct entry
-    //  do not exit early!  this change needs to be saved to the file system
-    _;
-
-  } else if (_) {
-    // step 9 c.1: make sure the value is defined
-    //  alert the user and exit early if they did not
-    if (_) {
-      console.log(`no value provided.  cannot write "${key}"`);
-      _;
-    }
-
-    // step 9 c.2:  write the key/value pair in entries
-    //  do not exit early!  this change needs to be saved to the file system
-    _;
+if (argv._.includes('delete')) {
+  if (argv.key === undefined) {
+    console.log("A key is needed");
+    process.exit(0);
   }
+  console.log('delete an entry ...');
+  fs.readFile(path.join(__dirname, ENTRIES_PATH), 'utf-8',
+    (err, contents) => {
+      if (err) throw err;
+      const parsedContent = JSON.parse(contents);
+      if (parsedContent[argv.key]) {
+        delete parsedContent[argv.key];
+        console.log(parsedContent);
+        const toWrite = JSON.stringify(parsedContent, null, '  ');
+        //now write the data again
+        fs.writeFile(path.join(__dirname, ENTRIES_PATH), toWrite, (err) => {
+          if (err)
+            throw err;
+          console.log("deletion finished...");
+        })
+      }
+      else {
+        console.log('No such a key found to delete ...');
+        process.exit(0);
+
+      }
+
+    });
+}
 
 
-  // step 10: convert the new entries object to a string
-  const newEntriesString = _;
 
-  // step 11: declare writeFileCallback
-  const writeFileCallback = (err) => {
-    // step 13: let the user know if their changes were successfully saved
-    if (_) { _ } else {
-      console.log('your changes were saved');
-      console.log(`(${command}) ${key}`);
-    };
-  };
+/*A summary of elements used in the program:
 
-  // step 12: save changes to the file system
-  _;
-
-};
-
-// step 2: declare callback that uses main app function
-const readFileCb = (err, entriesString) => {
-  // step 4: handle file system error, or execute main app function
-  if (_) {
-    _
-  }
-
-  const parsedEntries = _;
-  _(_, _, _, _);
-};
-
-// step 3: read the stored data and execute the callback
-_;
+    argv: This is the modified process.argv which
+    we have configured with yargs.
+    command(): This method is used to add commands,
+     their description and options which are specific to
+     these commands only, like in the above code lyr is the
+     command and -y
+    is lyr specific option: node myapp.js lyr -y 2016
+    option(): This method is used to
+    add global options(flags) which can be accessed by all
+    commands or without any command.
+    help(): This method is used to display a help dialogue
+    when --help option is encountered which contains description
+    of all the commands and options available.
+    alias(): This method provides an alias name to an option,
+    like in the above code both --help and -h triggers the help dialogue.
+*/
